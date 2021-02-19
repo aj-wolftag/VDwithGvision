ce6f9dea393e106664f889f261c8ed0e76f4461c
aj-wolftag <arjunalakshan56@gmail.com>
2021-02-20 01:44:10 +0530

    second commit

--- violation_detection.py
+++ violation_detection.py
@@ -1,9 +1,8 @@
 import cv2
-import imutils
-import easyocr
+from google.cloud import vision
 import numpy as np
 import time
-import os
+import os, io
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 import random
@@ -136,46 +135,7 @@ while True:
                     print("Vehicle Type: "+label)
                     
                     #ANPR BEGIN----------------------------------------------------------------------------
-                    img = cv2.imread(os.path.join(path,'vehicle_'+label+'_'+str(count)+'.jpg'))
-                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    #cv2.imshow('original img',gray)
-
-                    bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
-                    edged = cv2.Canny(bfilter, 30, 200) #Edge detection
-                    #cv2.imshow('Noise reduction img',edged)
-                    try:
-                        keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                        contours = imutils.grab_contours(keypoints)
-                        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
-
-                        location = None
-                        for contour in contours:
-                            approx = cv2.approxPolyDP(contour, 10, True)
-                            if len(approx) == 4:
-                                location = approx
-                                break
-
-                        #print(location)
-
-                        mask = np.zeros(gray.shape, np.uint8)
-                        new_image = cv2.drawContours(mask, [location], 0,255, -1)
-                        new_image = cv2.bitwise_and(img, img, mask=mask)
-                        #cv2.imshow('mask img',new_image)
-
-                        (x,y) = np.where(mask==255)
-                        (x1, y1) = (np.min(x), np.min(y))
-                        (x2, y2) = (np.max(x), np.max(y))
-                        cropped_image = gray[x1:x2+1, y1:y2+1]
-                        cv2.imshow('cropped img',cropped_image)
-
-                        reader = easyocr.Reader(['en'])
-                        result = reader.readtext(cropped_image)
-                        for (bbox, text, prob) in result:
-	                    # display the OCR'd text and associated probability 
-                            print("[INFO] {:.4f}: {}".format(prob, text))
-                        
-                    except:
-                        print("Unclear Vehicle Number-Plate!")
+                    
                     #ANPR END------------------------------------------------------------------------------
 
                     #recoding video
